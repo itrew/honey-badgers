e93d53d79d9e7b45e0b9ecd6563daab5fe7a5e18
Ian Trewella <Ian.Trewella@dfw.wa.gov>
2021-07-08 15:13:24 -0700

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="w-full min-h-screen relative grid grid-cols-1 grid-rows-3">
     <div class="text-center text-4xl tracking-widest uppercase font-bold text-white">
       <h1>Honey Badgers</h1>
-      <p>#Fearless, #Thick-skinned, #Resourcefull, #Unwaivering, #Ferocious.</p>
+      <p text-3xl>#Fearless, #Thick-skinned, #Resourcefull, #Unwaivering, #Ferocious.</p>
       <div class="flex justify-center items-center">
         <img class="row-start-2" src="https://static.dw.com/image/47406105_403.jpg">
       </div>
