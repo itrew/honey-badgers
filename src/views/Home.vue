9e819d69a5f93e9f338cfa78353a55183a540a2e
Ian Trewella <Ian.Trewella@dfw.wa.gov>
2021-07-08 16:00:17 -0700

    f

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="w-full min-h-screen relative grid grid-cols-1 grid-rows-3">
-    <div class="text-center text-4xl tracking-widest uppercase font-bold text-white">
-      <h1>Honey Badgers</h1>
-      <p text-3xl>#Fearless, #Thick-skinned, #Resourcefull, #Unwaivering, #Ferocious.</p>
-      <div class="flex justify-center items-center">
+  <div class="w-full relative grid grid-cols-1">
+    <div class="text-center tracking-widest uppercase font-bold text-white">
+      <h1 class="font-header font-thin text-5xl mt-8" style="color: yellow">Honey Badgers</h1>
+      <p class="text-xl mt-8">
+        #Fearless, #Thick-skinned, #Resourcefull, #Unwaivering, #Ferocious.
+      </p>
+      <div class="flex justify-center items-center mt-8">
         <img class="row-start-2" src="https://static.dw.com/image/47406105_403.jpg">
       </div>
+      <p class="text-l mt-8">
+        Are you ready to meet the fearless and ferocious yet resourceful honey
+        badger? Come explore our page to learn more and then come see our new exhibit
+        this fall
+      </p>
     </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        header: ['Zen Tokyo Zoo', 'sans-serif'],
+      },
       colors: {
         gunmetal: '#2D3142',
+        purple: '#9431b5',
       },
     },
   },
