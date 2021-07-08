7d68d7bf2d48cd30b14f3e8c3b3592682791485e
Ian Trewella <Ian.Trewella@dfw.wa.gov>
2021-07-08 14:49:40 -0700

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full min-h-screen relative grid grid-cols-1 grid-rows-3">
+    <div class="text-center text-4xl tracking-widest uppercase font-bold text-white">
+      <h1>Honey Badgers</h1>
+      <p>#Fearless, #Thick-skinned, #Resourcefull, #Unwaivering, #Ferocious.</p>
+      <div class="flex justify-center items-center">
+        <img class="row-start-2" src="https://static.dw.com/image/47406105_403.jpg">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {},
+};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gunmetal: '#2D3142',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
